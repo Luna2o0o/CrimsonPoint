850845106780c6602c454c687abf36c7bb34231a
Luna <litziluna04@gmail.com>
2025-05-23 11:45:56 -0400

    Update deploy_vm.sh

--- scripts/deploy_vm.sh
+++ scripts/deploy_vm.sh
@@ -11,17 +11,17 @@ AMI_ID=$(aws ec2 describe-images \
 aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t2.micro \
-    --key-name crimsionkey \
+    --key-name crimsonkey \
     --security-groups default \
     --user-data file://scripts/setup.sh \
     --region $REGION \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=oasis-bash-vm}]'
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=crimson-bash-vm}]'
 
-aws s3 mb s3://crimsion-logs-luna
-aws s3 cp logs/crimsion_log1.txt s3://crimsion-logs-luna/
+aws s3 mb s3://crimson-logs-luna
+aws s3 cp logs/crimson_log1.txt s3://crimsion-logs-luna/
 
-aws s3 mb s3://crimsion-sensor-logs
-aws s3 cp logs/ride_sensor_log.json s3://crimsion-sensor-logs/
+aws s3 mb s3://crimson-sensor-logs
+aws s3 cp logs/ride_sensor_log.json s3://crimson-sensor-logs/
 
 aws sns create-topic --name ride-alerts
 aws sns subscribe --topic-arn arn:aws:sns:us-east-1:<your-account-id>:ride-alerts \
