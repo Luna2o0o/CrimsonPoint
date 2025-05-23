a5c1853d07c57298915357b97446f6840bde68f8
Luna <litziluna04@gmail.com>
2025-05-23 11:44:51 -0400

    Update create_table.sh

--- scripts/create_table.sh
+++ scripts/create_table.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 aws dynamodb create-table \
-  --table-name crimsion-schedule \
+  --table-name crimson-schedule \
   --attribute-definitions AttributeName=task_id,AttributeType=S \
   --key-schema AttributeName=task_id,KeyType=HASH \
   --billing-mode PAY_PER_REQUEST
