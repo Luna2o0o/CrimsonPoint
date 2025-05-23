4302af284198208572132d8aff78f8db10e8e085
Luna <litziluna04@gmail.com>
2025-05-23 10:56:01 -0400

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Installing Git and Terraform..."
+sudo yum update -y
+sudo yum install -y git unzip
+
+# Install Terraform
+curl -O https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.zip
+unzip terraform_1.5.7_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+terraform -version
+
+echo "Cloning OutlawOasis repo..."
+git clone https://github.com/Luna2o0o/OutlawOasis.git
+cd OutlawOasis
+
+echo "Zipping Lambda function..."
+bash scripts/zip_lambda.sh
+
+echo "Creating DynamoDB table..."
+bash scripts/create_table.sh
+
+echo "Uploading schedule item..."
+bash scripts/upload_schedule.sh
+
+echo "Applying Terraform..."
+cd terraform
+terraform init
+terraform apply -auto-approve
