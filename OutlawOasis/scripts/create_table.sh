6688b0132f2ea385e7936263fe01261afaa4415b
Luna <litziluna04@gmail.com>
2025-05-23 09:57:46 -0400

    Create create_table.sh

---
+++ OutlawOasis/scripts/create_table.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+aws dynamodb create-table \
+  --table-name outlaw-schedule \
+  --attribute-definitions AttributeName=task_id,AttributeType=S \
+  --key-schema AttributeName=task_id,KeyType=HASH \
+  --billing-mode PAY_PER_REQUEST
