0c75e8fcf7e41baa10cd3a6efa90d452d4267ed2
Luna <litziluna04@gmail.com>
2025-05-23 11:46:49 -0400

    Update upload_schedule.sh

--- scripts/upload_schedule.sh
+++ scripts/upload_schedule.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws dynamodb put-item \
-  --table-name outlaw-schedule \
+  --table-name crimson-schedule \
   --item file://../data/schedule.json \
   --return-consumed-capacity TOTAL
