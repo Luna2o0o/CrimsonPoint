11dba31e8dd42366fc706c584a4d62a1841db47a
Luna <litziluna04@gmail.com>
2025-05-23 11:38:21 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region to deploy resources in"
+  default     = "us-east-1"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket to store sensor logs"
+  default     = "outlaw-sensor-logs"
+}
