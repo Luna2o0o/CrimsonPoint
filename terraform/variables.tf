4a98018c573c6c28dc34a75c9b49d71c6fafd1f5
Luna <litziluna04@gmail.com>
2025-05-27 16:42:55 -0400

    Delete terraform/variables.tf

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "AWS region to deploy resources in"
-  default     = "us-east-1"
-}
-
-variable "s3_bucket_name" {
-  description = "Name of the S3 bucket to store sensor logs"
-  default     = "crimson-sensor-logs"
-}
