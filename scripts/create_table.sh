9261091f6ff964aa1dd1bbc488747261ac429d6b
Luna <litziluna04@gmail.com>
2025-05-27 16:44:25 -0400

    Delete scripts/create_table.sh

--- scripts/create_table.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws dynamodb create-table \
-  --table-name crimson-schedule \
-  --attribute-definitions AttributeName=task_id,AttributeType=S \
-  --key-schema AttributeName=task_id,KeyType=HASH \
-  --billing-mode PAY_PER_REQUEST
