862f11988a0ddb144521944d060b24eccadbeede
Luna <litziluna04@gmail.com>
2025-05-27 16:16:36 -0400

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,13 +1,19 @@
+# Run a script to fetch the current public IP
+data "external" "my_ip" {
+  program = ["bash", "${path.module}/get_ip.sh"]
+}
+
 resource "aws_security_group" "crimson_sg" {
   name        = "crimson-sg"
-  description = "Allow SSH from my IP"
+  description = "Allow SSH from my current IP"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
+    description = "SSH access from my machine"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR.IP.ADDRESS.HERE/32"] # Replace with your IP
+    cidr_blocks = ["${data.external.my_ip.result.ip}/32"]
   }
 
   egress {
