1dba87202fdf723a8431239a739cc2801bd16e5c
Luna <litziluna04@gmail.com>
2025-05-27 16:07:14 -0400

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "crimson_sg" {
+  name        = "crimson-sg"
+  description = "Allow SSH from my IP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR.IP.ADDRESS.HERE/32"] # Replace with your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "crimson-sg"
+  }
+}
