180baacf9f7b9e0aad552e575bef75a3370688fa
Luna <litziluna04@gmail.com>
2025-06-02 01:53:28 -0400

    Update glue_crawler.tf

--- terraform/glue_crawler.tf
+++ terraform/glue_crawler.tf
@@ -1,18 +1,16 @@
-## 2. Terraform: glue_crawler.tf
-# Automates creation of an AWS Glue Crawler for S3 logs
-
 resource "aws_glue_catalog_database" "crimson_logs" {
   name = "crimson_logs_db"
 }
 
 resource "aws_glue_crawler" "crimson_log_crawler" {
-  name         = "crimson-log-crawler"
-  role         = aws_iam_role.glue_crawler_role.arn
+  name          = "crimson-log-crawler"
+  role          = aws_iam_role.glue_crawler_role.arn
   database_name = aws_glue_catalog_database.crimson_logs.name
+
   s3_target {
-    path = "s3://crimson-point-logs2025/"
+    path = "s3://${var.log_bucket_name}/"
   }
-  schedule = "cron(0 * * * ? *)" # every hour (optional)
+
   schema_change_policy {
     update_behavior = "UPDATE_IN_DATABASE"
     delete_behavior = "DEPRECATE_IN_DATABASE"
