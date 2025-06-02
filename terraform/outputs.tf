1f7709eb2d71fe2377ffc920d65720e8a4ca9fae
Luna <litziluna04@gmail.com>
2025-06-02 01:58:20 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,12 @@ output "public_ip" {
 output "s3_bucket" {
   value = aws_s3_bucket.crimson_logs.bucket
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.crimson_vm.public_ip
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.equipment_usage.name
+}
+
