f890b8780428f106bc90578521b9c32d1e44d518
Luna <litziluna04@gmail.com>
2025-06-06 12:34:26 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,14 @@
-output "public_ip" {
-  value = aws_instance.crimson_vm.public_ip
+output "autoscaling_group_name" {
+  description = "The name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.crimson_asg.name
 }
 
-output "s3_bucket" {
-  value = aws_s3_bucket.crimson_logs.bucket
+output "launch_template_id" {
+  description = "The ID of the EC2 launch template"
+  value       = aws_launch_template.crimson_template.id
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.crimson_vm.public_ip
+output "s3_log_bucket" {
+  description = "The name of the S3 bucket for logs"
+  value       = aws_s3_bucket.crimson_logs.bucket
 }
-
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.equipment_usage.name
-}
-
