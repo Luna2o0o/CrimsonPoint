8d2d099e8e07015887959f16cc08a4c12fa002ab
Luna <litziluna04@gmail.com>
2025-06-12 17:14:13 -0400

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,22 +18,12 @@ resource "aws_lambda_function" "log_parser" {
   depends_on = [aws_iam_role.lambda_s3_role]
 }
 
-resource "aws_s3_bucket_notification" "bucket_notify" {
-  bucket = aws_s3_bucket.crimson_logs.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.log_parser.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_suffix       = ".txt"
-  }
-
-  depends_on = [aws_lambda_permission.allow_s3]
-}
-
-resource "aws_lambda_permission" "allow_s3" {
-  statement_id  = "AllowExecutionFromS3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.log_parser.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.crimson_logs.arn
+# ✅ Add sensor_alert Lambda right below
+resource "aws_lambda_function" "sensor_alert" {
+  function_name = "sensor_alert"
+  handler       = "sensor_alert.lambda_handler"
+  runtime       = "python3.12"
+  filename      = "${path.module}/../lambda/sensor_alert.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambda/sensor_alert.zip")
+  role          = aws_iam_role.lambda_s3_role.arn
 }
